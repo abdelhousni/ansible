6869f7fae526233099ba68c106891901e330f08d
Student User <student@workstation.lab.example.com>
2019-04-04 06:04:00 -0400

    Tweaks during testing

--- linux/do447/gitlab.rb
+++ linux/do447/gitlab.rb
@@ -1,6 +1,6 @@
 external_url 'http://utility.lab.example.com:8081'
 nginx['listen_port'] = 8081
-unicorn['port'] = 8082
+unicorn['port'] = 8083
 nginx['redirect_http_to_https'] = false
 gitlab_rails['ldap_enabled'] = true
 gitlab_rails['ldap_servers'] = YAML.load_file('/etc/gitlab/freeipa_settings.yml')
